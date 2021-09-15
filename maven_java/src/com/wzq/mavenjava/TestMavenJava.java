ecbe2a59ef9f162dc5d47acf4962437344c37345
wangzhiqiang <wangzhiqiangcom@163.com>
2021-09-15 18:07:18 +0800

    newIdea first commit

---
+++ maven_java/src/com/wzq/mavenjava/TestMavenJava.java
@@ -0,0 +1,12 @@
+package com.wzq.mavenjava;
+
+public class TestMavenJava {
+    public static void main(String[] args) {
+        System.out.println("中");
+        if(6 > 9){
+            System.out.println("666");
+        }else{
+            System.out.println("999");
+        }
+    }
+}
---
+++ src/com/wzq/bean/HelloNewIdea.java
@@ -0,0 +1,7 @@
+package com.wzq.bean;
+
+public class HelloNewIdea {
+    public static void main(String[] args) {
+        System.out.println("Hello IDEA 2019,this is WangZhiqiang !");
+    }
+}
